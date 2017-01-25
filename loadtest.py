b969b12d41868839c8963af4ec05a9c089eeb617
Tarek Ziade <tarek@ziade.org>
2017-01-25 21:14:39 +0100

    stub demo

--- loadtest.py
+++ loadtest.py
@@ -1,11 +1,20 @@
 import os
-from molotov import scenario
+from molotov import scenario, setup
 
 
 _SERVER = os.getenv('SERVER', 'http://localhost:8080')
+_STUB = 'https://www-demo4.allizom.org/en-US/firefox/stub_attribution_code/'
+
+
+@setup()
+async def _init(args):
+    return {}
 
 
 @scenario(10)
 async def scenario_one(session):
-    async with session.get(_SERVER) as resp:
+    headers = {'X-Requested-With': 'XMLHttpRequest'}
+    query = '?utm_source=1&utm_medium=2&utm_referrer=ok'
+    async with session.get(_STUB + query, headers=headers) as resp:
+        result = await resp.json()
         assert resp.status == 200
