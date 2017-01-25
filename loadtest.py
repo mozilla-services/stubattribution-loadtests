76a17a6753d5f964981b201ac8e583cd55f9d19c
Tarek Ziade <tarek@ziade.org>
2017-01-25 20:25:26 +0100

    boilerplate for a molotov test

--- loadtest.py
+++ loadtest.py
@@ -1,3 +1,11 @@
 import os
+from molotov import scenario
 
-from molotov.fmwk import scenario
+
+_SERVER = os.getenv('SERVER', 'http://localhost:8080')
+
+
+@scenario(10)
+async def scenario_one(session):
+    async with session.get(_SERVER) as resp:
+        assert resp.status == 200
